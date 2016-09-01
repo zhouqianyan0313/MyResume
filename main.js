7824400583a9130b02b0e1909c00da59544a352b
zhouqiaoyan0313 <823770029@qq.com>
2016-09-01 23:02:37 +0800

    test update

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-window.onload(function(){
-	var lis=get
-})
+window.onload=function(){
+	var ul=document.getElementsByTagName("ul")[0];
+	var lis=ul.getElementsByTagName("li");
+	var oInformation=document.getElementById("information");
+	var oDivs=oInformation.getElementsByTagName("div");
+
+	for (var i=0; i<lis.length; i++){
+		lis[i].index=i;
+		lis[i].onclick=function(){
+			for (var j=0; j<oDivs.length; j++){
+				oDivs[j].style.display="none";
+			}
+			oDivs[this.index].style.display="block";
+		}
+	}
+
+}
